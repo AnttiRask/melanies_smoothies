3f92972a0786ac11a4ebfc6677c0980670ce6c81
Antti Rask <anttilennartrask@gmail.com>
2024-05-28 10:30:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,4 @@ if ingredients_list:
 # New section to display fruityvice nutrition information
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_Width=TRUE)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=TRUE)
