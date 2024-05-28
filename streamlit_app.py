51b8907cfe65037f941ec2bc96a10863960ab6f1
Antti Rask <anttilennartrask@gmail.com>
2024-05-28 10:26:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -44,3 +45,7 @@ if ingredients_list:
 
         success_text = f"Your Smoothie is ordered, {name_on_order.strip()}!"
         st.success(success_text, icon="✅")
+
+# New section to display fruityvice nutrition information
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
